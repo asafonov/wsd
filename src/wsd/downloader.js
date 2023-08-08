dfcbfdd13500fef1200e517d745aa71e2b5fe6ef
asafonov <me@asafonov.org>
2023-08-08 11:26:16 +0200

    download complete

--- src/wsd/downloader.js
+++ src/wsd/downloader.js
@@ -1,16 +1,78 @@
 const fs = require('fs')
+const http = require('https')
+const binaryFiles = ['.jpg', '.jpeg', '.gif', '.png', '.mp3', '.ogg', '.mp4']
+const urls = {}
+const dirnames = {}
+const files = {}
+const downloads = {}
 
-const get = async (domain, filename) => {
-  const url = `https://${domain}/${filename}`
-  const dirName = `${domain}/${filename}`.replace(/\/[^\/]+\.[^\/]+$/, '').replace(/^\/+/, '')
-  let file = filename.match(/[^\/]+\.[^\/]+$/)
-  file = file ? file[0] : 'index.html'
+const getKey = (domain, filename) => {
+  return `${domain}/${filename}`
+}
+
+const getUrl = (domain, filename) => {
+  const key = getKey(domain, filename)
+
+  if (! urls[key]) {
+    urls[key] = `https://${domain}/${filename}`
+  }
+
+  return urls[key]
+}
+
+const getDirname = (domain, filename) => {
+  const key = getKey(domain, filename)
+
+  if (! dirnames[key]) {
+    dirnames[key] = `${domain}/${filename}`.replace(/\/[^\/]+\.[^\/]+$/, '').replace(/^\/+/, '')
+  }
+
+  return dirnames[key]
+}
+
+const getFile = (filename) => {
+  if (! files[filename]) {
+    const file = filename.match(/[^\/]+\.[^\/]+$/)
+    files[filename] = file ? file[0] : 'index.html'
+  }
+
+  return files[filename]
+}
+
+const get = (domain, filename) => {
+  const url = getUrl(domain, filename)
+
+  if (downloads[urls]) return
+
+  const dirname = getDirname(domain, filename)
+  const file = getFile(filename)
 
   try {
-    fs.mkdirSync(dirName, {recursive: true})
+    fs.mkdirSync(dirname, {recursive: true})
   } catch {}
 
   console.log(`Getting ${url}`)
+
+  const f = fs.createWriteStream(`${dirname}/${file}`)
+  const request = http.get(url, response => {
+    response.pipe(f)
+    f.on('finish', () => {
+      f.close()
+      console.log(`${dirname}/${file} saved`)
+      downloads[url] = true
+      parse(domain, filename, response.headers['content-type'])
+    })
+  })
+}
+
+const parse = (domain, filename, contentType) => {
+  if (contentType.indexOf('text') === false) {
+    return
+  }
+
+  const dirname = getDirname(domain, filename)
+  const file = getFile(filename)
+  const content = fs.readFileSync(`${dirname}/${file}`).toString()
 }
 
 module.exports = {
