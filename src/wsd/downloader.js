8e9f45871d59d23376977f46dfeca00646347048
asafonov <me@asafonov.org>
2023-08-08 13:23:14 +0200

    parse urls

--- src/wsd/downloader.js
+++ src/wsd/downloader.js
@@ -73,6 +73,12 @@ const parse = (domain, filename, contentType) => {
   const dirname = getDirname(domain, filename)
   const file = getFile(filename)
   const content = fs.readFileSync(`${dirname}/${file}`).toString()
+  const m = content.matchAll(/<a[^>]+href=['"](.*?)['"]/g)
+  const urls = []
+
+  for (let i of m) {
+    urls.push(i[1])
+  }
 }
 
 module.exports = {
